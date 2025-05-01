9753987f01f7b8a88599e799dc5ca4a66388c2c6
Captain Sero <yasoo976098@gmail.com>
2025-05-01 23:07:18 +0300

    hhhh

--- app1/lib/main.dart
+++ app1/lib/main.dart
@@ -10,16 +10,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter Demo'),
-        ),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Flutter Demo')),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: const <Widget>[
-              Text('Hello World!'),
-            ],
+            children: const <Widget>[Text('Hiiiii')],
           ),
         ),
       ),

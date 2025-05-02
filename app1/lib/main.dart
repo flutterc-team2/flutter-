c1366aeb77d4a158e0d8974294ff268c26ad1e8f
Captain Sero <yasoo976098@gmail.com>
2025-05-02 12:37:31 +0300

    login

--- app1/lib/main.dart
+++ app1/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:app1/classes/responsive_ui.dart';
+import 'package:app1/screens/login_screen.dart';
 import 'package:app1/widgets/pageview.dart';
 import 'package:flutter/material.dart';
 
@@ -20,6 +21,7 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
 
+      // home: LoginScreen(),
       home: Scaffold(
         backgroundColor: Colors.white,
         body: Column(
--- app1/lib/screens/login_screen.dart
+++ app1/lib/screens/login_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:app1/classes/responsive_ui.dart';
 import 'package:flutter/material.dart';
 
@@ -53,20 +55,123 @@ class _LoginScreenState extends State<LoginScreen> {
                 Container(
                   width: ResponsiveUi.width * 0.8,
                   height: ResponsiveUi.height * 0.05,
-                  color: Colors.grey,
-                  child: TextFormField(),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Color(0xffC4C4C4),
+                  ),
+
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Email",
+                      suffixIcon: Icon(Icons.email),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      enabledBorder: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                      contentPadding: EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 10,
+                      ),
+                    ),
+                  ),
                 ),
 
                 SizedBox(height: 20),
 
-                Align(
-                  alignment: Alignment.center,
-                  child: Container(
-                    width: ResponsiveUi.width * 0.8,
-                    height: ResponsiveUi.height * 0.05,
-                    color: Colors.grey,
-                    child: TextFormField(),
+                Container(
+                  width: ResponsiveUi.width * 0.8,
+                  height: ResponsiveUi.height * 0.05,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Color(0xffC4C4C4),
                   ),
+
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Password",
+                      suffixIcon: Icon(Icons.lock),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      enabledBorder: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                      contentPadding: EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 10,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          Positioned(
+            top: ResponsiveUi.height / 1.6,
+            left: ResponsiveUi.width / 12,
+            child: Row(
+              children: [
+                Checkbox(value: false, onChanged: (_) {}),
+                Text(
+                  "Remember me",
+                  style: TextStyle(
+                    color: Color(0xff252525),
+                    fontSize: 12,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(width: ResponsiveUi.width * 0.2),
+
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Forgot Password?",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 12,
+                      color: Color(0xff6C63FF),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          Positioned(
+            bottom: ResponsiveUi.height / 20,
+            left: ResponsiveUi.width / 6,
+            child: Column(
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xff6C63FF),
+                    minimumSize: Size(
+                      ResponsiveUi.width * 0.7,
+                      ResponsiveUi.height * 0.05,
+                    ),
+                  ),
+                  child: Text("Login", style: TextStyle(color: Colors.white)),
+                ),
+
+                SizedBox(height: ResponsiveUi.height * 0.02),
+                Row(
+                  children: [
+                    Text("New Member?"),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Register Now",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 12,
+                          color: Color(0xff6C63FF),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),

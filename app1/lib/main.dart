ccaf837a288236e559730392d2e51d11f574f539
Captain Sero <yasoo976098@gmail.com>
2025-05-02 22:35:01 +0300

    hhhhh

--- app1/lib/main.dart
+++ app1/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:app1/classes/responsive_ui.dart';
 import 'package:app1/screens/show_installments.dart';
+import 'package:app1/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,6 +18,6 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     ResponsiveUi.initial(context: context);
-    return MaterialApp(debugShowCheckedModeBanner: false, home: ShowInstallments());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: SplashScreen());
   }
 }
--- app1/lib/screens/splash_screen.dart
+++ app1/lib/screens/splash_screen.dart
@@ -31,20 +31,8 @@ class _SplashScreenState extends State<SplashScreen> {
           children: [
             Image.asset("assets/onboarding_3.png", width: 150, height: 150), // add pathe the image
             const SizedBox(height: 20),
-            const Text(
-              "Welcome to the Quiz App!",
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 10),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: Text(
-                "This app helps users answer fun and educational quizzes with images and voice assistance.",
-                style: TextStyle(fontSize: 16),
-                textAlign: TextAlign.center,
-              ),
-            ),
+          
+            
             const SizedBox(height: 20),
             const CircularProgressIndicator(),
           ],
